44a3d95cc561fa8103839797950e9ac3359ba815
weliton jose <welitonjose@192.168.0.4>
2025-02-04 16:30:23 -0300

    loader

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,13 +1,10 @@
 import { Application } from "@hotwired/stimulus"
-import $ from 'jquery';
 
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
+application.debug = true
 window.Stimulus   = application
-window.$ = $;
-console.log("jquery")
-console.log($)
+
 
 export { application }
